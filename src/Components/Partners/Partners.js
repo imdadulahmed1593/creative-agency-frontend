99a0f0e3971773479f5d95e856813fcb94ccba3c
IMDAD <ahmedimdad1593@gmail.com>
2020-10-16 22:12:09 +0600

    responsive done

--- src/Components/Partners/Partners.js
+++ src/Components/Partners/Partners.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import "./Partners.css"
+import React from "react";
+import "./Partners.css";
 function Partners() {
-    return (
-        <div className="partners__logos d-flex mt-5 justify-content-center align-items-center container row">
-        <div className="col-sm partners__logo"><img src={require("../../images/logos/google.png")} alt=""/></div>
-        <div className="col-sm partners__logo"><img src={require("../../images/logos/netflix.png")} alt=""/></div>
-        <div className="col-sm partners__logo"><img src={require("../../images/logos/airbnb.png")} alt=""/></div>
-        <div className="col-sm partners__logo"><img src={require("../../images/logos/slack.png")} alt=""/></div>
-        <div className="col-sm partners__logo"><img src={require("../../images/logos/uber.png")} alt=""/></div>
-            
-        </div>
-    )
+  return (
+    <div className="partners__logos d-flex mt-5 justify-content-center align-items-center container row">
+      <div className="col-sm partners__logo">
+        <img src={require("../../images/logos/google.png")} alt="" />
+      </div>
+      <div className="col-sm partners__logo">
+        <img src={require("../../images/logos/netflix.png")} alt="" />
+      </div>
+      <div className="col-sm partners__logo">
+        <img src={require("../../images/logos/airbnb.png")} alt="" />
+      </div>
+      <div className="col-sm partners__logo">
+        <img src={require("../../images/logos/slack.png")} alt="" />
+      </div>
+      <div className="col-sm partners__logo">
+        <img src={require("../../images/logos/uber.png")} alt="" />
+      </div>
+    </div>
+  );
 }
 
-export default Partners
+export default Partners;
